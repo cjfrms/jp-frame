0b07d08cc54a2acfd7071cd43eb40649b65bda0e
geuni <cjfrms@live.com>
2018-08-30 17:51:52 +0800

    x

--- jp-admin/src/main/java/com/jptech/jpframe/admin/comm/DynamicDataSourceAspect.java
+++ jp-admin/src/main/java/com/jptech/jpframe/admin/comm/DynamicDataSourceAspect.java
@@ -6,9 +6,12 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
 
 @Aspect
 @Component
@@ -31,5 +34,24 @@ public class DynamicDataSourceAspect {
     public void doAfter(JoinPoint joinPoint, TargetDataSource targetDataSource) {
         logger.info(String.format("当前数据源  %s  执行清理方法", targetDataSource.id()));
         DataSourceContextHolder.clear();
+    }
+
+    @Before(value = "pointCut()")
+    public void doBeforeWithSlave(JoinPoint joinPoint) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        //获取当前切点方法对象
+        Method method = methodSignature.getMethod();
+        if (method.getDeclaringClass().isInterface()) {//判断是否为借口方法
+            try {
+                //获取实际类型的方法对象
+                method = joinPoint.getTarget().getClass()
+                        .getDeclaredMethod(joinPoint.getSignature().getName(), method.getParameterTypes());
+            } catch (NoSuchMethodException e) {
+                logger.error("方法不存在！", e);
+            }
+        }
+        if (null == method.getAnnotation(TargetDataSource.class)) {
+            //DataSourceContextHolder.setSlave();
+        }
     }
 }
